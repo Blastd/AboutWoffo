ab9ad59bac0dc3619f391c06e3d31c3bdd22168e
Drew <carullic@outlook.it>
2025-08-25 00:06:10 +0200

    Added tome scrolling animation

--- src/components/Scene.tsx
+++ src/components/Scene.tsx
@@ -1,12 +1,35 @@
 import { Canvas } from "@react-three/fiber";
-import WoffoHead from "./WoffoHead";
+import { Tome } from "./Tome";
+import { PerspectiveCamera } from "@react-three/drei";
+import { Suspense, useEffect, useRef, useState } from "react";
+import { Color } from "three";
 
 export default function Scene() {
+    const canvasRef = useRef<HTMLCanvasElement>(null!);
+    const [shouldOpen, setShouldOpen] = useState(false);
+    useEffect(()=> {
+        const height = Math.max(document.documentElement.clientHeight || 0, window.innerHeight || 0);
+        document.addEventListener('scroll', () => {
+            const box = canvasRef.current.getBoundingClientRect();
+            setShouldOpen(box.top >= 0 && box.bottom <= height)
+        })
+    }, [])
+
     return <>
-        <Canvas>
-            <ambientLight />
-            <pointLight position={[10, 10, 10]}/>
-            <WoffoHead/>
+        <Canvas ref={canvasRef} style={{scrollSnapAlign: "start"}}>
+            <PerspectiveCamera makeDefault
+                position={[0, 95.5, 11.5]}
+                rotation={[-4*Math.PI/9, 0, 0]}/>
+            <ambientLight intensity={1}
+                color={new Color(0xA2DDD6)}/>
+            <spotLight position={[0, 70, 0]}
+                intensity={2000}
+                distance={10000}
+                angle={0.96}
+                decay={1.5}/>
+            <Suspense fallback={null}>
+                <Tome castShadow shouldOpen={shouldOpen}/>  
+            </Suspense>
         </Canvas>
     </>
 }
